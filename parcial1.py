9f62b47be4559185b2f1474c78348a7c8f463924
Koenig Juan <koenigjuan@gmail.com>
2026-04-10 15:37:50 -0300

    creacion de menu

---
+++ parcial1.py
@@ -0,0 +1,3 @@
+#voy a empezar por el menu, despues hago las estructuras while y las restricciones
+
+eleccion = int(input("1)Carga de herramientas\n2)Carga de existencias\n3)Ver inventario\n4)Consulta de stock\n5)Agotados\n6)Nuevo producto"))
