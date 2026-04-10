feaa0108b091a0a99fff800bbef6ac04d9cef23e
Koenig Juan <koenigjuan@gmail.com>
2026-04-10 16:01:52 -0300

    comentarios guia para no tener que ver el pdf

--- parcial1.py
+++ parcial1.py
@@ -1,3 +1,10 @@
 #voy a empezar por el menu, despues hago las estructuras while y las restricciones
 
-eleccion = int(input("1)Carga de herramientas\n2)Carga de existencias\n3)Ver inventario\n4)Consulta de stock\n5)Agotados\n6)Nuevo producto"))
+eleccion = int(input("1)Carga de herramientas\n2)Carga de existencias\n3)Ver inventario\n4)Consulta de stock\n5)Agotados\n6)Nuevo producto\n7)Actualizar stock (compra/venta)\n8)Salir\n"))
+
+
+
+
+
+print(eleccion)
+print("FIN")
